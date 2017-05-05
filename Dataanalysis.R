3e867b9e1e2a70371e843b10bfddc7f54bd32807
Zhengguo Gu <z.gu@uvt.nl>
2017-05-05 09:53:00 +0200

    update

--- Dataanalysis.R
+++ Dataanalysis.R
@@ -114,7 +114,7 @@ while(num_test <= 360){
 colnames(IPR_reg) <- c("1%", "5%", "10%", "25%", "50%", "75%", "90%", "95%", "99%")
 colnames(IPR_Tscore) <- c("1%", "5%", "10%", "25%", "50%", "75%", "90%", "95%", "99%")
 
-save(IPR_reg, IPR_Tscore, file = "20170405firstrun.RData")
+save(IPR_reg, IPR_Tscore, REL_changescore, file = "20170405firstrun.RData")
 ########################## PART II:  checking reproducability  #########################################
 #### This is to double check the reproducability of the Rcode "Dataanalysis.R" 
 ####
